ee86d60f4d92e260b5da0d4784456c23cd33cab9
Samuel Husso <shusso@iki.fi>
2016-03-02 18:09:20 +0200

    send balls in space

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,16 +5,19 @@ using weapons;
 public class Player : MonoBehaviour {
 	public GameObject cannonPrefab;
 
-	Vector3 velocity = new Vector3(0, 20, 0);
+	Vector3 velocity = new Vector3(0, 0, 400);
 
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.Mouse0)) {
-			print("mouse pressed");
-			
-			var s = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			print("mouse pressed " +Input.mousePosition.ToString());
 
+			var l = new Vector3(Input.mousePosition.x,
+				Input.mousePosition.y, 8);
+			
+			var s = Camera.main.ScreenToWorldPoint(l);
 			var cannon = Instantiate(cannonPrefab, s, Quaternion.identity) as GameObject;
 			cannon.GetComponent<Rigidbody>().AddForce(velocity);
+			Destroy(cannon, 6.0f);
 		}
 		
 	}
